4452b92d13c10c8e68c71fc8a114bea0dc9e4fae
Liyu <liyuumk@gmail.com>
2023-06-08 23:56:54 +0300

    url input field

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -3,13 +3,47 @@ import { signIn, signOut, useSession } from "next-auth/react";
 import Head from "next/head";
 import Link from "next/link";
 import { api } from "@/utils/api";
+import { DocumentDuplicateIcon } from "@heroicons/react/24/outline";
 
 const Home: NextPage = () => {
   const hello = api.example.hello.useQuery({ text: "from tRPC" });
 
   return (
-    <div className=" flex h-screen items-center justify-center ">
-      <div>{hello.data?.greeting ?? "No data"}</div>
+    <div className="flex h-screen flex-col items-center justify-center p-5">
+      <div className="w-full max-w-xl">
+        <InputUrlForm />
+      </div>
+      <div className="mt-2 flex w-full max-w-xl justify-between bg-slate-600 p-2">
+        <p className="text-slate-300">Hey this is your url copy it</p>
+        <DocumentDuplicateIcon
+          onClick={() => {
+            console.log("Do copy");
+          }}
+          className="h-6 w-6 text-slate-100 hover:cursor-pointer"
+        />
+      </div>
+    </div>
+  );
+};
+
+const InputUrlForm = () => {
+  return (
+    <div className="flex rounded bg-slate-100">
+      <input
+        type="text"
+        id="first_name"
+        className="block w-full rounded-l bg-white p-2.5 text-sm text-gray-900  outline-0 outline-slate-600 dark:bg-white dark:placeholder-slate-400"
+        placeholder="http://yourURL"
+        required
+        autoComplete="off"
+      />
+
+      <button
+        type="button"
+        className="rounded-r bg-slate-100 px-3.5 py-2.5 text-slate-800 shadow-sm hover:bg-slate-200"
+      >
+        Shorten!
+      </button>
     </div>
   );
 };
