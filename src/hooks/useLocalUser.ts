d76bc47d89022cae1c25f4312bfd0f10b625f9b3
Liyu M <liyuumk@gmail.com>
2023-06-23 03:00:47 +0300

    Limit initial render checker for userLocalUser just to dev

--- src/hooks/useLocalUser.ts
+++ src/hooks/useLocalUser.ts
@@ -3,7 +3,7 @@ import { getEnv } from "@/utils/getEnv";
 import { useEffect } from "react";
 
 export const useLocalUser = () => {
-  // const { isDev } = getEnv();
+  const { isDev } = getEnv();
 
   const { mutate, data, isLoading } = api.user.createTemporary.useMutation();
 
@@ -11,7 +11,7 @@ export const useLocalUser = () => {
 
   useEffect(() => {
     const localUserId = localStorage.getItem("luid");
-    if (initialRender) {
+    if (initialRender && isDev) {
       initialRender = false;
       return;
     }
